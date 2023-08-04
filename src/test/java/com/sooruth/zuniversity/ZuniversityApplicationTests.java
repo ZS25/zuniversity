81cae8bc78ae39aa336d8195eac319cd0d1488ef
zulfekar <17052721+ZS25@users.noreply.github.com>
2023-08-03 22:59:51 -0400

    Created Zuniversity Application

---
+++ src/test/java/com/sooruth/zuniversity/ZuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sooruth.zuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
