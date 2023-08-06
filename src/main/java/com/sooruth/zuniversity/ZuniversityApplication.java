49c61bd1289cfa24c4154d42fdeb42a7866ba559
zulfekar <17052721+ZS25@users.noreply.github.com>
2023-08-06 19:24:28 -0400

    Added Swagger

--- src/main/java/com/sooruth/zuniversity/ZuniversityApplication.java
+++ src/main/java/com/sooruth/zuniversity/ZuniversityApplication.java
@@ -1,9 +1,29 @@
 package com.sooruth.zuniversity;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Zuniversity application",
+                version = "0.0.1-SNAPSHOT",
+                description = "This project is made to try out the new features of Spring Boot 3.2 over Java 21",
+                termsOfService = "sooruth",
+                contact = @Contact(
+                        name = "Zulfekar",
+                        email = "https://www.linkedin.com/in/zulfekarsooruth"
+                ),
+                license = @License(
+                        name = "License",
+                        url = "https://github.com/ZS25"
+                )
+        )
+)
 public class ZuniversityApplication {
 
     public static void main(String[] args) {
