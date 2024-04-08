c40d5f0f985af72ae96c719f513fe23c969c1d20
zulfekar <17052721+ZS25@users.noreply.github.com>
2024-04-08 00:39:10 -0400

    Customised welcome message in HomepageController.java and upgraded snapshot to 0.0.5

--- src/main/java/com/sooruth/zuniversity/controller/HomepageController.java
+++ src/main/java/com/sooruth/zuniversity/controller/HomepageController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/")
-public class MainController {
+public class HomepageController {
 
     @Value("${zuni.welcomeMessage: default welcome message}")
     private String welcomeMessage;
     @GetMapping
-    public String returnsString(){
-        return welcomeMessage;
+    public String homePage(Principal principal){
+        return String.format(welcomeMessage, principal.getName());
     }
 }
